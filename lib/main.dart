c7122d617a91484de8fa47380306a78b8c65e3e1
Hana Lee <i@leehana.co.kr>
2020-09-10 23:37:25 +0900

    * add play sound function

--- lib/main.dart
+++ lib/main.dart
@@ -4,19 +4,68 @@ import 'package:audioplayers/audio_cache.dart';
 void main() => runApp(XylophoneApp());
 
 class XylophoneApp extends StatelessWidget {
+  void playSound(int soundNumber) {
+    final player = AudioCache();
+    player.play('note$soundNumber.wav');
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         body: SafeArea(
-          child: Center(
-            child: FlatButton(
-              onPressed: () {
-                final player = AudioCache();
-                player.play('note1.wav');
-              },
-              child: Text('Click Me!'),
-            ),
+          child: Column(
+            children: <Widget>[
+              FlatButton(
+                color: Colors.red,
+                onPressed: () {
+                  playSound(1);
+                },
+                child: Text('도'),
+              ),
+              FlatButton(
+                color: Colors.orange,
+                onPressed: () {
+                  playSound(2);
+                },
+                child: Text('레'),
+              ),
+              FlatButton(
+                color: Colors.yellow,
+                onPressed: () {
+                  playSound(3);
+                },
+                child: Text('미'),
+              ),
+              FlatButton(
+                color: Colors.green,
+                onPressed: () {
+                  playSound(4);
+                },
+                child: Text('파'),
+              ),
+              FlatButton(
+                color: Colors.blue,
+                onPressed: () {
+                  playSound(5);
+                },
+                child: Text('솔'),
+              ),
+              FlatButton(
+                color: Colors.indigo,
+                onPressed: () {
+                  playSound(6);
+                },
+                child: Text('라'),
+              ),
+              FlatButton(
+                color: Colors.purple,
+                onPressed: () {
+                  playSound(7);
+                },
+                child: Text('시'),
+              ),
+            ],
           ),
         ),
       ),
